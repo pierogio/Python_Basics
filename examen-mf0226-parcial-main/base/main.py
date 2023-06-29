043265c108111a1c1f6e8e06a79d0e955973e5cc
Piero Giorgio <113985355+pierogio@users.noreply.github.com>
2023-06-29 18:21:00 +0200

    Add files via upload

---
+++ examen-mf0226-parcial-main/base/main.py
@@ -0,0 +1,19 @@
+# TODO completa la suma
+def sum(a, b):
+    return a +
+
+
+# TODO prepara una función que multiplica dos números que recibe por argumento
+def mult(a, b):
+
+
+# TODO prepara una función que genere números dentro de una lista
+
+
+if __name__ == '__main__':
+    # TODO llama a la función suma y printa el resultado por pantalla
+    # TODO llama la función multiplicar y printa el resultado por pantalla
+    print(mult(, ))
+    # TODO llama a la función de generar numeros y printa la lista generada
+
+
